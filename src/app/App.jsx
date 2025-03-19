5864bffac5984f867c789bb919131bc6b513e067
Donghyeop <dxxxhxxxx@gmail.com>
2025-03-19 17:31:54 +0900

    feat: vite config setting

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 function App() {
-    return <h1>Vite</h1>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <h1>Vite</h1>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
